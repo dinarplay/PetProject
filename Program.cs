9401e009fccf75033f6973611391f6bea3549bc7
Dinarchlk <57683662+dinarplay@users.noreply.github.com>
2022-12-02 16:36:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using PetProject.Models;
+using PetProject.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddMvc();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Auth/Login";
+        options.AccessDeniedPath = "/Home/Index";
+    }); // "Cookie"
+builder.Services.AddAuthorization();
+
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddTransient<PassGeneratorService>();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints( endpoints =>
+{
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
---
+++ wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿window.onload = function () {
+    $('html, html *').contents().each(function () {
+        if (this.nodeType === 3) { // text node
+            this.textContent = this.textContent.replace(/\u00A0/g, '');
+        }
+    });
+}
+
